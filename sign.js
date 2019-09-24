641263e181b597c9992d810067b7fb5442177e77
Anmol Jain <you@example.com>
2019-09-24 11:56:16 +0800

    commit by anmol

--- sign.js
+++ sign.js
@@ -3,11 +3,11 @@ var confirm_password = document.getElementById("confirm_pwd");
 
 function validatePassword(){
   if(pwd.value != confirm_pwd.value) {
-    confirm_password.setCustomValidity("Passwords Don't Match");
+    confirm_password.setCustomValidity("Passwords Don't Match!");
   } else {
     confirm_password.setCustomValidity('');
   }
 }
 
-password.onchange = validatePassword;
-confirm_password.onkeyup = validatePassword;
+password.onchange = validatePassword();
+confirm_password.onkeyup = validatePassword();
